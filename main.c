d31dde5763bf495af0578a12223d68e17277d5fa
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2022-10-21 20:28:07 +0200

    Add files via upload
    Playing with pointers and comparing two char
    Planning to comparing to "strings" in C without using strcmp

--- main.c
+++ main.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  
-  float average;
-  int i;
-  int j;
-  int grades[2][5];  
 
-  grades[0][0] = 80;
-  grades[0][1] = 70;
-  grades[0][2] = 65;
-  grades[0][3] = 89;
-  grades[0][4] = 90;
+  char name1 = 'A';
+  char *pName1 = &name1;
 
-  grades[1][0] = 85;
-  grades[1][1] = 80;
-  grades[1][2] = 80;
-  grades[1][3] = 82;
-  grades[1][4] = 87;
+  char name2 = 'A';
+  char *pName2 = &name2;
 
-  for(i=0; i<2; i++){
-    average = 0;
-    for(j=0; j<5; j++){
-      average += grades[i][j];
-    };
+  char test1 = *pName1;
+  char test2 = *pName2;
 
-    average = sizeof(grades)/sizeof(grades[0][0]);  
-    printf("Average is : %.4g\n", average);  
+  if(test1 == test2){
+    printf("Good");
+  } else {
+    printf("Nope");
   };
-
+  
   return 0;
 }
